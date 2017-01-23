1542c7aeaf2b231c694f9244cabb4a195781cdd2
Alexis Barnes <alexisbarnes@dhcp-v511-200185.resnet-1x-wireless.unc.edu>
2017-01-22 21:06:31 -0500

    Images for fruits and desserts are showing. No results yet.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.info('Welcome to the dessert maker v1.0.');
 
-var currentFruit = null;
-var currentDessert = null;
+var currentFruit = " ";
+var currentDessert = " ";
 
 var images = {
   fruits: {
@@ -33,8 +33,51 @@ var images = {
   }
 };
 
+
 var apple = document.querySelector('.Fruit--apple');
 apple.addEventListener('click', function() {
   console.log('Apple clicked.');
-  // update image in fruit holder
+  document.querySelector('.Holder--fruit').innerHTML= '<img src="' + images.fruits.apple + '">';
+  // console.log(images.fruits.apple);
+  currentFruit = "Apple";
 });
+
+var orange = document.querySelector('.Fruit--orange');
+orange.addEventListener('click', function() {
+  console.log('Orange clicked.');
+  document.querySelector('.Holder--fruit').innerHTML= '<img src="' + images.fruits.orange + '">';
+  // console.log(images.fruits.apple);
+});
+
+var banana = document.querySelector('.Fruit--banana');
+banana.addEventListener('click', function() {
+  console.log('Banana clicked.');
+  document.querySelector('.Holder--fruit').innerHTML= '<img src="' + images.fruits.banana + '">';
+  // console.log(images.fruits.apple);
+});
+
+var icecream = document.querySelector('.Dessert--icecream');
+icecream.addEventListener('click', function() {
+  console.log('Ice Cream clicked.');
+  document.querySelector('.Holder--dessert').innerHTML= '<img src="' + images.desserts.icecream + '">';
+  // console.log(images.fruits.apple);
+  currentDessert = "IceCream";
+});
+
+var cake = document.querySelector('.Dessert--cake');
+cake.addEventListener('click', function() {
+  console.log('Cake clicked.');
+  document.querySelector('.Holder--dessert').innerHTML= '<img src="' + images.desserts.cake + '">';
+  // console.log(images.fruits.apple);
+});
+
+var pie = document.querySelector('.Dessert--smoothie');
+pie.addEventListener('click', function() {
+  console.log('Pie clicked.');
+  document.querySelector('.Holder--dessert').innerHTML= '<img src="' + images.desserts.pie + '">';
+  // console.log(images.fruits.apple);
+});
+
+if (currentFruit === "Apple" && currentDessert === "IceCream") {
+  var appleicecream = document.querySelector('.Holder--result').innerHTML='<img src"' + images.mixes.apple.icecream + '">';
+}
